2d23c2cde39ea8849b078a857d10d0b684b241f3
yjw5344 <yjw5344@hanmail.net>
2018-04-12 17:58:58 +0900

    homework waterFall

--- routes/login/signup.js
+++ routes/login/signup.js
@@ -8,47 +8,66 @@ var UserData = require('../../config/dbconfig');
 //node와 몽고디비의 인터페이스
 
 router.post('/', function(req, res, next) {
-    if(err){
-      res.status(500).send({
-         stat : "fail",
-         msgs : "hashing fail"
-      });
-    }else{
-        var item = {
-          name: req.body.name,
-          email: req.body.email,
-          nickname: req.body.nickname,
-          password: hashed.toString('base64'),
-          posi: req.body.posi,
-          mKind: req.body.mKind,
-          mServiceStartDate: req.body.mServiceStartDate,
-          gender: req.body.gender,
-          salt: salt
-        };
-        var data = new UserData(item);
-        data.save((err) => {
-            if (err) {
-                res.status(500).send({
-                    stat:"fail",
-                    message:"sign up fail"
-                });
-            } else {
-                res.status(201).send({
-                    stat: "success",
-                    data: {
-                        name: req.body.name,
-                        email: req.body.email,
-                        nickname: req.body.nickname,
-                        posi: req.body.posi,
-                        mKind: req.body.mKind,
-                        mServiceStartDate: req.body.mServiceStartDate,
-                        gender: req.body.gender
-                    }
-                });
-            }
-        });
-    }
-
+  let taskArray = [
+      (callback) => {
+          let salt = crypto.randomBytes(32).toString('base64');
+          crypto.pbkdf2(req.body.password, salt, 100000, 64, 'sha512', (err, hashed) => {
+              if (err) {
+                  res.status(500).send({
+                      stat: "fail",
+                      msgs: "hashing fail"
+                  });
+                  callback("hashing fail" + err);
+              } else {
+                  callback(null, hashed, salt);
+              }
+          });
+      },
+      (hashed, salt, callback) => {
+          var item = {
+              name: req.body.name,
+              email: req.body.email,
+              nickname: req.body.nickname,
+              password: hashed.toString('base64'),
+              posi: req.body.posi,
+              mKind: req.body.mKind,
+              mServiceStartDate: req.body.mServiceStartDate,
+              gender: req.body.gender,
+              salt: salt
+          };
+          callback(null, item);
+      },
+      (item, callback) => {
+          var data = new UserData(item);
+          data.save((err) => {
+              if (err) {
+                  res.status(500).send({
+                      stat: "fail",
+                      message: "sign up fail"
+                  });
+                  callback("sign up fail" + err, null);
+              } else {
+                  res.status(201).send({
+                      stat: "success",
+                      data: {
+                          name: req.body.name,
+                          email: req.body.email,
+                          nickname: req.body.nickname,
+                          posi: req.body.posi,
+                          mKind: req.body.mKind,
+                          mServiceStartDate: req.body.mServiceStartDate,
+                          gender: req.body.gender
+                      }
+                  });
+                  callback("sign up success", null);
+              }
+          });
+      }
+  ];
+  async.waterfall(taskArray, (err, result)=>{
+      if(err) console.log(err);
+      else console.log(result);
+  });
 
 });
 
